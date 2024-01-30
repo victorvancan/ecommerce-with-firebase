ecc810f03b00c0266c4a841535f4216281bc32de
victor vançan <74362179+victorvancan@users.noreply.github.com>
2024-01-29 21:52:39 -0300

    home curved edges

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,3 +1,5 @@
+import 'package:ecommerce/common/widgets/custom_shapes/containers/circular_container.dart';
+import 'package:ecommerce/common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
 import 'package:ecommerce/utils/constants/colors.dart';
 import 'package:flutter/material.dart';
 
@@ -7,17 +9,38 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
+        body: SingleChildScrollView(
+      child: Column(
+        children: [
+          TPrimaryHeaderContainer(
+            child: Container(
               color: TColors.primary,
               padding: const EdgeInsets.all(0),
-              
-            )
-          ],
-        ),
-      )
-    );
+              child: SizedBox(
+                height: 400,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      top: -150,
+                      right: -250,
+                      child: TCircularContainer(
+                        backgroundColor: TColors.white.withOpacity(0.1),
+                      ),
+                    ),
+                    Positioned(
+                      top: -100,
+                      right: -300,
+                      child: TCircularContainer(
+                        backgroundColor: TColors.white.withOpacity(0.1),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    ));
   }
 }
